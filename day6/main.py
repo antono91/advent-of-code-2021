98cd17ec057e5e5ea68bd7229d69d75263f3f189
Anton <anton.gundermann@gmx.de>
2021-12-09 11:27:27 +0100

    Add files via upload

--- day6/main.py
+++ day6/main.py
@@ -1,1 +1,16 @@
+def get_input(file):
+    with open(file) as f:
+        return [int(x) for x in f.readline().strip().split(',')]
 
+
+def solve(puzzle, days):
+    state = [puzzle.count(i) for i in range(9)]
+    for _ in range(days):
+        state = state[1:] + [state[0]]
+        state[6] += state[8]
+    return sum(state)
+
+
+puzzle = get_input('input.txt')
+print(solve(puzzle, 80))
+print(solve(puzzle, 256))
