94a6d8b1abbe95a833ff8773e54e1e0d516fac2c
Anton <anton.gundermann@gmx.de>
2021-12-09 11:13:58 +0100

    Add files via upload

--- day2/main.py
+++ day2/main.py
@@ -1,1 +1,26 @@
+def get_input(file):
+    with open(file) as f:
+        return [(line.split()[0], int(line.split()[1])) for line in f]
 
+
+def solve(puzzle):
+    horizontal = depth = 0
+    for direction, value in puzzle:
+        if direction == "forward": horizontal += value
+        elif direction == "up": depth -= value
+        else: depth += value
+    return horizontal * depth
+
+def solve2(puzzle):
+    horizontal = depth = aim = 0
+    for direction, value in puzzle:
+        if direction == "forward":
+            horizontal += value
+            depth += aim * value
+        elif direction == "up": aim -= value
+        else: aim += value
+    return horizontal * depth
+
+puzzle = get_input('input.txt')
+print(solve(puzzle))
+print(solve2(puzzle))
